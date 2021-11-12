f4c5aedc44b4b0651815d64a37da96816b2f42ba
elliotfincher <93586520+elliotfincher@users.noreply.github.com>
2021-11-12 17:52:03 +0000

    ValueError logic on every guess
    The ValueError logic now works no matter which step the user enters a non-conforming value.

--- guessing_game.py
+++ guessing_game.py
@@ -5,14 +5,14 @@ Project 1 - Number Guessing Game
 """
 
 import random
+import math
 
 
 
 def start_game():
     
-    #Assign variables that will be referenced later
-    highscore=10000000000  
-    
+    #Assign highscore to be referenced later - infinity value as it will be overwritten with something lower.
+    highscore=math.inf
     #Welcome message
     print("Welcome to the Elliot's number guessing game!")
        
@@ -20,7 +20,6 @@ def start_game():
     
       randnum=random.randint(1,10)
       number_of_attempts=1
-      
       player_guess=input("Please guess a number: ")
       
       while True:
@@ -35,39 +34,38 @@ def start_game():
           print("Please ensure you enter a number between 1 and 10")
           number_of_attempts += 1
           player_guess=input("Please guess a number: ")
+          continue
         else:
-          break
-      
-      while (player_guess != randnum):
-        if (player_guess > randnum):
-          print("It's lower")
-          player_guess=input("Please guess a number: ")
-          player_guess=int(player_guess)
-          number_of_attempts += 1
-        else:
-          print("It's higher")
-          player_guess=input("Please guess a number: ")
-          player_guess=int(player_guess)
-          number_of_attempts +=1
-      else:
-        print("You got it!")
-        print("You took {} attempts".format(number_of_attempts))
-        if(number_of_attempts < highscore):
-          highscore = number_of_attempts
+            if (player_guess == randnum):
+              break
+            elif (player_guess > randnum):
+              print("It's lower")
+              number_of_attempts += 1
+              player_guess=input("Please guess a number: ")
+              continue
+            else:
+              print("It's higher")
+              number_of_attempts +=1
+              player_guess=input("Please guess a number: ")
+              continue
         
+      print("You got it!")
+      print("You took {} attempts".format(number_of_attempts))
+      if(number_of_attempts < highscore):
+        highscore = number_of_attempts  
         
-        playagain=input("Would you like to play again? (y/n) ")    
+      playagain=input("Would you like to play again? (y/n) ")    
         #ensure no value other than "y" and "n" can be entered
         
-        while (playagain.lower() != "y") and (playagain.lower() != "n"):
-          playagain=input("Please choose either 'y' or 'n': ")
+      while (playagain.lower() != "y") and (playagain.lower() != "n"):
+        playagain=input("Please choose either 'y' or 'n': ")
+      else:
+        if(playagain.lower() == "n"):
+          print("Thank you for playing the number guessing game :)")
+          break
         else:
-          if(playagain.lower() == "n"):
-            print("Thank you for playing the number guessing game :)")
-            break
-          else:
-            print("The highscore is {} attempts".format(highscore))
-            continue
+          print("The highscore is {} attempts".format(highscore))
+          continue
           
 
 # Kick off the program by calling the start_game function.
